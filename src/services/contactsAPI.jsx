c252302771fb7741a004b6d8252669fa84112da4
Lart28 <saref28@gmail.com>
2024-05-27 23:29:57 +0300

    edit API url

--- src/services/contactsAPI.jsx
+++ src/services/contactsAPI.jsx
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://66006fc487c91a1164195496.mockapi.io/contacts';
+axios.defaults.baseURL = 'https://6654ec943c1d3b602937cdd0.mockapi.io/contacts';
 
 export async function fetchContacts() {
-    const { data } = await axios.get('/contacts');
-    return data;
+  const { data } = await axios.get('/contacts');
+  return data;
 }
 
-export async function addContact({name, phone}) {
-    const { data } = await axios.post('/contacts', {
-        name,
-        phone,
-    });
-    return data; 
+export async function addContact({ name, phone }) {
+  const { data } = await axios.post('/contacts', {
+    name,
+    phone,
+  });
+  return data;
 }
 
 export async function deleteContact(id) {
-    const { data } = await axios.delete(`/contacts/${id}`)
-    return data;
+  const { data } = await axios.delete(`/contacts/${id}`);
+  return data;
 }
